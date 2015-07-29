ed10c9304c7a6f127f58d0fcdc2528fc400e8044
Jeremy Holland <jeremyholland@Jeremys-MacBook-Pro.local>
2015-07-28 22:51:25 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+//http://stackoverflow.com/questions/15158937/jquery-jump-or-scroll-to-certain-position-div-or-target-on-the-page-from-button
+
+$(document).ready(function(){
+
+
+$(".jumper").on("click", function( e ) {
+
+        e.preventDefault();
+
+        $("body, html").animate({ 
+            scrollTop: $( $(this).attr('href') ).offset().top 
+        }, 600);
+
+    });
+
+})
